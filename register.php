4fb527353c860aee491617d66f31af6bb8fa9084
Aditya76-reza <iadityareza@gmail.com>
2024-07-13 12:28:01 +0700

    notif bisa di close
    menambahkan script agar notifikasi pada register bisa di close

--- register.php
+++ register.php
@@ -146,5 +146,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
+    <script>
+        // Script untuk menutup notifikasi dengan tombol close
+        $(document).ready(function(){
+            $(".alert .close").click(function(){
+                $(this).parent().fadeOut("slow");
+            });
+        });
+    </script>
 </body>
 </html>
