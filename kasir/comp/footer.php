4b7a69db01aa7e59cccdd900e1fa42ca063f2f66
dhitoadhitya <ekaadhitya69@gmail.com>
2024-07-13 21:46:22 +0700

    Update footer.php
    menambahkan background footer

--- kasir/comp/footer.php
+++ kasir/comp/footer.php
@@ -1,6 +1,6 @@
 
 <!-- /.content-wrapper -->
-<footer class="main-footer" style="background-image: url('asset/img/5.jpg'); background-size: cover; background-position: center;">
+  <footer class="main-footer" style="background-image: url('asset/img/5.jpg'); background-size: cover; background-position: center;">
     <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Zibran | Powered By <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
