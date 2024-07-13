9136e51ed54ab24c56346be21fad5e785e538204
dhitoadhitya <ekaadhitya69@gmail.com>
2024-07-13 21:58:13 +0700

    Update awal.php
    menambah kan href

--- kasir/awal.php
+++ kasir/awal.php
@@ -38,7 +38,7 @@ $row = mysqli_fetch_array($select_2);
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="index.php">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
             </ol>
           </div><!-- /.col -->
