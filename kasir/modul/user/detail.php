67ed88df946503279798c0ad0a2a0e72c4374ef6
dhitoadhitya <ekaadhitya69@gmail.com>
2024-07-13 22:51:41 +0700

    merubah background halaman profile
    merubah warna background halaman profile menjadi gradien warna hijau dan putih

--- kasir/modul/user/detail.php
+++ kasir/modul/user/detail.php
@@ -18,7 +18,7 @@ if (isset($_POST['edit'])) {
 ?>
 
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+<div class="content-wrapper" style="background: linear-gradient(to right,green,white); ;">
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
